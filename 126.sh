85e6e4c7925f518dbcc0e7e8745c975ce3679c16
prasadabhisek6 <106363574+prasadabhisek6@users.noreply.github.com>
2022-06-05 12:05:53 +0530

    Create 126.sh

---
+++ 126.sh
@@ -0,0 +1,16 @@
+ echo “Enter a number”
+ read num
+ case $num in
+ [0-9])
+ echo “you have entered a single digit number”
+ ;;
+ [1-9][1-9])
+ echo “you have entered a two-digit number”
+ ;;
+ [1-9][1-9][1-9])
+ echo “you have entered a three-digit number”
+ ;;
+ *)
+ echo “your entry does not match any of the conditions”
+ ;;
+ Esac
